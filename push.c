3a27d98bbe004ede15233737704206bc8db561b0
cedrickkouek <cedrickkouek@gmail.com>
2023-11-17 01:50:08 +0100

    Got it

--- push.c
+++ push.c
@@ -7,23 +7,33 @@
  *Return: no return
  */
 
-void c_push(stack_t **stack, unsigned int n)
+void f_push(stack_t **head, unsigned int counter)
 {
-	stack_t *new_node;
+	int n, j = 0, flag = 0;
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	if (bus.arg)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-
-	new_node->n = n;
-
-	new_node->next = *stack;
-	new_node->prev = NULL;
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
